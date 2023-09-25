88fd053cef9f5c62af9367f7927ee29bd8cc653d
Armin Sabouri <armins88@gmail.com>
2023-09-25 09:13:38 -0400

    fix(rpc): fix botanix rpc warnings

--- crates/rpc/rpc/src/eth/api/mod.rs
+++ crates/rpc/rpc/src/eth/api/mod.rs
@@ -23,7 +23,7 @@ use reth_primitives::{
 use reth_provider::{
     BlockReaderIdExt, ChainSpecProvider, EvmEnvProvider, StateProviderBox, StateProviderFactory,
 };
-use reth_rpc_types::{GatewayAddress, SyncInfo, SyncStatus};
+use reth_rpc_types::{SyncInfo, SyncStatus};
 use reth_tasks::{TaskSpawner, TokioTaskExecutor};
 use reth_transaction_pool::TransactionPool;
 use std::{
