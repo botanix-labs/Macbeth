6eda5d706a6d546c88c7b49a1b65a9c1b0ca8613
Armin Sabouri <armins88@gmail.com>
2023-10-26 16:21:55 -0400

    scaffolding for authority concensus storage

--- crates/consensus/authority/src/lib.rs
+++ crates/consensus/authority/src/lib.rs
@@ -11,8 +11,8 @@
     attr(deny(warnings, rust_2018_idioms), allow(dead_code, unused_variables))
 ))]
 
-//! A [Consensus] implementation for local testing purposes
-//! that automatically seals blocks.
+//! A [Consensus] implementation of Clique Proof of Authority (POA)
+//! that authoritymatically seals blocks.
 //!
 //! The Mining task polls a [MiningMode], and will return a list of transactions that are ready to
 //! be mined.
@@ -20,16 +20,34 @@
 //! These downloaders poll the miner, assemble the block, and return transactions that are ready to
 //! be mined.
 use reth_consensus_common::validation;
-use reth_interfaces::consensus::{Consensus, ConsensusError};
-use reth_primitives::{
-    constants::{ALLOWED_FUTURE_BLOCK_TIME_SECONDS, MAXIMUM_EXTRA_DATA_SIZE},
-    Chain, ChainSpec, Hardfork, Header, SealedBlock, SealedHeader, EMPTY_OMMER_ROOT, U256,
+use reth_interfaces::{
+    consensus::{Consensus, ConsensusError},
+    executor::{BlockExecutionError, BlockValidationError},
 };
-use std::{sync::Arc, time::SystemTime};
+use reth_primitives::{
+    constants::{
+        ALLOWED_FUTURE_BLOCK_TIME_SECONDS, EMPTY_RECEIPTS, EMPTY_TRANSACTIONS,
+        ETHEREUM_BLOCK_GAS_LIMIT, MAXIMUM_EXTRA_DATA_SIZE,
+    },
+    proofs, Address, Block, BlockBody, BlockHash, BlockHashOrNumber, BlockNumber, Bloom, Chain,
+    ChainSpec, Header, ReceiptWithBloom, SealedBlock, SealedHeader, TransactionSigned,
+    EMPTY_OMMER_ROOT, H256, U256, Hardfork,
+};
+use reth_provider::{PostState, StateProvider};
+use reth_revm::executor::Executor;
+use reth_transaction_pool::TransactionPool;
+use std::{
+    collections::HashMap,
+    sync::Arc,
+    time::{SystemTime, UNIX_EPOCH},
+};
+use voting::AuthorityVote;
 
+use tokio::sync::{RwLock, RwLockReadGuard, RwLockWriteGuard};
+use tracing::{trace, warn};
 mod builder;
-mod voting;
 mod utils;
+mod voting;
 pub use builder::AuthorityConsensusBuilder;
 
 /// Ethereum authority consensus
@@ -138,5 +156,228 @@ fn validate_header_extradata(header: &Header) -> Result<(), ConsensusError> {
         Err(ConsensusError::ExtraDataExceedsMax { len: header.extra_data.len() })
     } else {
         Ok(())
+    }
+}
+
+/// In memory storage
+#[derive(Debug, Clone, Default)]
+pub(crate) struct Storage {
+    inner: Arc<RwLock<StorageInner>>,
+}
+
+// == impl Storage ===
+impl Storage {
+    fn new(header: SealedHeader) -> Self {
+        let (header, best_hash) = header.split();
+        let mut storage = StorageInner {
+            best_hash,
+            total_difficulty: header.difficulty,
+            best_block: header.number,
+            ..Default::default()
+        };
+        storage.headers.insert(0, header);
+        storage.bodies.insert(best_hash, BlockBody::default());
+        Self { inner: Arc::new(RwLock::new(storage)) }
+    }
+
+    /// Returns the write lock of the storage
+    pub(crate) async fn write(&self) -> RwLockWriteGuard<'_, StorageInner> {
+        self.inner.write().await
+    }
+
+    /// Returns the read lock of the storage
+    pub(crate) async fn read(&self) -> RwLockReadGuard<'_, StorageInner> {
+        self.inner.read().await
+    }
+}
+
+/// In-memory storage for the chain the authority seal engine is building.
+#[derive(Default, Debug)]
+pub(crate) struct StorageInner {
+    /// Headers buffered for download.
+    pub(crate) headers: HashMap<BlockNumber, Header>,
+    /// A mapping between block hash and number.
+    pub(crate) hash_to_number: HashMap<BlockHash, BlockNumber>,
+    /// Bodies buffered for download.
+    pub(crate) bodies: HashMap<BlockHash, BlockBody>,
+    /// Tracks best block
+    pub(crate) best_block: u64,
+    /// Tracks hash of best block
+    pub(crate) best_hash: H256,
+    /// The total difficulty of the chain until this block
+    pub(crate) total_difficulty: U256,
+    /// Keep track of current votes
+    pub(crate) authority_votes: Vec<AuthorityVote>,
+}
+
+// === impl StorageInner ===
+
+impl StorageInner {
+    /// Returns the block hash for the given block number if it exists.
+    pub(crate) fn block_hash(&self, num: u64) -> Option<BlockHash> {
+        self.hash_to_number.iter().find_map(|(k, v)| num.eq(v).then_some(*k))
+    }
+
+    /// Returns the matching header if it exists.
+    pub(crate) fn header_by_hash_or_number(
+        &self,
+        hash_or_num: BlockHashOrNumber,
+    ) -> Option<Header> {
+        let num = match hash_or_num {
+            BlockHashOrNumber::Hash(hash) => self.hash_to_number.get(&hash).copied()?,
+            BlockHashOrNumber::Number(num) => num,
+        };
+        self.headers.get(&num).cloned()
+    }
+
+    /// Inserts a new header+body pair
+    pub(crate) fn insert_new_block(&mut self, mut header: Header, body: BlockBody) {
+        header.number = self.best_block + 1;
+        header.parent_hash = self.best_hash;
+
+        self.best_hash = header.hash_slow();
+        self.best_block = header.number;
+        self.total_difficulty += header.difficulty;
+
+        trace!(target: "consensus::authority", num=self.best_block, hash=?self.best_hash, "inserting new block");
+        self.headers.insert(header.number, header);
+        self.bodies.insert(self.best_hash, body);
+        self.hash_to_number.insert(self.best_hash, self.best_block);
+    }
+
+    /// Fills in pre-execution header fields based on the current best block and given
+    /// transactions.
+    pub(crate) fn build_header_template(
+        &self,
+        transactions: &Vec<TransactionSigned>,
+        chain_spec: Arc<ChainSpec>,
+    ) -> Header {
+        // check previous block for base fee
+        let base_fee_per_gas = self
+            .headers
+            .get(&self.best_block)
+            .and_then(|parent| parent.next_block_base_fee(chain_spec.base_fee_params));
+
+        let mut header = Header {
+            parent_hash: self.best_hash,
+            ommers_hash: EMPTY_OMMER_ROOT,
+            beneficiary: Default::default(),
+            state_root: Default::default(),
+            transactions_root: Default::default(),
+            receipts_root: Default::default(),
+            withdrawals_root: None,
+            logs_bloom: Default::default(),
+            difficulty: U256::from(2),
+            number: self.best_block + 1,
+            gas_limit: ETHEREUM_BLOCK_GAS_LIMIT,
+            gas_used: 0,
+            timestamp: SystemTime::now().duration_since(UNIX_EPOCH).unwrap_or_default().as_secs(),
+            mix_hash: Default::default(),
+            nonce: 0,
+            base_fee_per_gas,
+            blob_gas_used: None,
+            excess_blob_gas: None,
+            extra_data: Default::default(),
+            parent_beacon_block_root: None,
+        };
+
+        header.transactions_root = if transactions.is_empty() {
+            EMPTY_TRANSACTIONS
+        } else {
+            proofs::calculate_transaction_root(transactions)
+        };
+
+        header
+    }
+
+    /// Executes the block with the given block and senders, on the provided [Executor].
+    ///
+    /// This returns the poststate from execution and post-block changes, as well as the gas used.
+    pub(crate) fn execute<DB: StateProvider>(
+        &mut self,
+        block: &Block,
+        executor: &mut Executor<DB>,
+        senders: Vec<Address>,
+        recent_block_header: Option<bitcoin::block::Header>,
+    ) -> Result<(PostState, u64), BlockExecutionError> {
+        trace!(target: "consensus::authority", transactions=?&block.body, "executing transactions");
+
+        let (post_state, gas_used) =
+            executor.execute_transactions(block, U256::ZERO, Some(senders), recent_block_header)?;
+
+        // apply post block changes
+        let post_state = executor.apply_post_block_changes(block, U256::ZERO, post_state)?;
+
+        Ok((post_state, gas_used))
+    }
+
+    /// Fills in the post-execution header fields based on the given PostState and gas used.
+    /// In doing this, the state root is calculated and the final header is returned.
+    pub(crate) fn complete_header<DB: StateProvider>(
+        &self,
+        mut header: Header,
+        post_state: &PostState,
+        executor: &mut Executor<DB>,
+        gas_used: u64,
+    ) -> Header {
+        let receipts = post_state.receipts(header.number);
+        header.receipts_root = if receipts.is_empty() {
+            EMPTY_RECEIPTS
+        } else {
+            let receipts_with_bloom =
+                receipts.iter().map(|r| r.clone().into()).collect::<Vec<ReceiptWithBloom>>();
+            header.logs_bloom =
+                receipts_with_bloom.iter().fold(Bloom::zero(), |bloom, r| bloom | r.bloom);
+            proofs::calculate_receipt_root(&receipts_with_bloom)
+        };
+
+        header.gas_used = gas_used;
+
+        // calculate the state root
+        let state_root = executor.db().db.0.state_root(post_state.clone()).unwrap();
+        header.state_root = state_root;
+        header
+    }
+
+    /// Builds and executes a new block with the given transactions, on the provided [Executor].
+    ///
+    /// This returns the header of the executed block, as well as the poststate from execution.
+    pub(crate) fn build_and_execute<DB: StateProvider>(
+        &mut self,
+        transactions: Vec<TransactionSigned>,
+        executor: &mut Executor<DB>,
+        chain_spec: Arc<ChainSpec>,
+        recent_block_header: Option<bitcoin::block::Header>,
+    ) -> Result<(SealedHeader, PostState), BlockExecutionError> {
+        let header = self.build_header_template(&transactions, chain_spec);
+
+        let block = Block { header, body: transactions, ommers: vec![], withdrawals: None };
+
+        let senders = TransactionSigned::recover_signers(&block.body, block.body.len())
+            .ok_or(BlockExecutionError::Validation(BlockValidationError::SenderRecoveryError))?;
+
+        trace!(target: "consensus::authority", transactions=?&block.body, "executing transactions");
+
+        // now execute the block
+        let (post_state, gas_used) =
+            self.execute(&block, executor, senders, recent_block_header)?;
+
+        let Block { header, body, .. } = block;
+        let body = BlockBody { transactions: body, ommers: vec![], withdrawals: None };
+
+        trace!(target: "consensus::authority", ?post_state, ?header, ?body, "executed block, calculating state root and completing header");
+
+        // fill in the rest of the fields
+        let header = self.complete_header(header, &post_state, executor, gas_used);
+
+        trace!(target: "consensus::authority", root=?header.state_root, ?body, "calculated root");
+
+        // finally insert into storage
+        self.insert_new_block(header.clone(), body);
+
+        // set new header with hash that should have been updated by insert_new_block
+        let new_header = header.seal(self.best_hash);
+
+        Ok((new_header, post_state))
     }
 }
