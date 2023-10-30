c17d3524f3bea5ebb001e28f11bb57c0cb02dcae
Scott Millner <22253830+scottmillner@users.noreply.github.com>
2023-10-30 15:42:34 -0600

    feature: add read_staker_balance to authority utils

--- crates/consensus/authority/src/utils.rs
+++ crates/consensus/authority/src/utils.rs
@@ -1,8 +1,15 @@
 use botanix_lib::extra_data_header::ExtraDataHeader;
-use reth_primitives::{Header, H256};
+use reth_primitives::{constants::{ STAKING_CONTRACT_ADDRESS, STAKER_BALANCE_MAPPING_STORAGE_SLOT_INDEX }, Address, Header, H256};
+use reth_provider::StateProvider;
 
 pub enum CreateSigHashError {
     
+}
+
+#[derive(Debug, Error)]
+pub enum StateProviderError {
+    #[error("Storage Access Error")]
+    StorageAccessError(&'static str),
 }
 
 /// Create sighash for authority to sign
@@ -10,4 +17,17 @@ pub fn create_authority_sighash(header: &Header, extra_data: &ExtraDataHeader) -
     header.extra_data = extra_data.serialize_without_signature().as_slice();
 
     Ok(header.hash_slow())
+}
+
+/// Read staker balance from staking contract storage
+pub fn read_staker_balance(provider: StateProvider, staker_address: Address) -> Result<U256, StorageAccessError> {
+    let staking_contract_address = Address::from_slice(&STAKING_CONTRACT_ADDRESS);
+    let staker_balance_mapping_storage_slot_index = STAKER_BALANCE_MAPPING_STORAGE_SLOT_INDEX;
+
+    let storage_key = keccak256(&[staker_address, staker_balance_mapping_storage_slot_index]);
+    let balance = provider.storage(staking_contract_address, storage_key).map_err(|_e| {
+        StateProviderError::StorageAccessError("Failed to access staking contract mapping storage slot");
+    })?;
+    
+    Ok(balance)
 }
