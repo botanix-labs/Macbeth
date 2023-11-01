14cb555a7adb3d3046e6bf7bee7b24533abc6e10
Armin Sabouri <armins88@gmail.com>
2023-11-01 16:43:42 -0400

    fix: instantiate epoch manager in authority consensus builder

--- crates/consensus/authority/src/epoch_manager.rs
+++ crates/consensus/authority/src/epoch_manager.rs
@@ -1,45 +1,48 @@
 use reth_primitives::constants::eip225::BLOCK_PERIOD;
 use reth_transaction_pool::{ValidPoolTransaction, TransactionPool};
-use tokio::time::Interval;
+use tokio::time::{Interval, Instant};
 
-use crate::constants;
-use crate::storage::Storage;
+use crate::Storage;
 use std::{
     sync::Arc,
-    task::{Context, Poll},
+    task::{Context, Poll}, time::Duration,
 };
 
 #[derive(Debug)]
 /// Manages the block production epochs
-pub struct EpochManager {
+/// Signing time is the parent timestamp + BLOCK_PERIOD
+/// If the signer is inturn then we broadcast the block
+/// If the signer is not inturn then we wait random amount time
+///
+/// Blocks will be rejected by consensus if 
+/// 1. The signer is not in the federation
+/// 2. The signer has broadcasted > 1 in SIGNER_LIMIT consecutive blocks
+pub(crate) struct EpochManager {
     /// Access to storage to fetch headers.
     // TODO (armins) this should be protected by an Arc.
-    storage: Storage,
+    pub(crate) storage: Storage,
 
     /// Pollable interval to lock nodes proposing for a min time defined by `BLOCK_PERIOD`.
-    proposal: Interval,
+    pub(crate) proposal_interval: Interval,
 }
 
 impl EpochManager {
-    pub fn new(&mut self, storage: Storage) -> Self {
-        let timestamp = storage.headers.get(&storage.best_block).timestamp;
-        let proposal = self.calculate_optimal_time_naively(timestamp);
-        Self { storage, proposal }
-    }
-
-    fn calculate_optimal_time_naively(&mut self, timestamp: u64) {
-        self.proposal = tokio::time::interval_at(timestamp, BLOCK_PERIOD);
+    pub fn naive_inverval(storage: Storage) -> Self {
+        let start = Instant::now();
+        let proposal_interval = tokio::time::interval_at(start, Duration::from_millis(BLOCK_PERIOD) );
+        Self { storage, proposal_interval }
     }
 
     pub(crate) fn poll<Pool>(
         &mut self,
         pool: &Pool,
         cx: &mut Context<'_>,
-    ) -> Pool<Vec<Arc<ValidPoolTransaction<<Pool as TransactionPool>::Transaction>>>>
+    ) -> Poll<Vec<Arc<ValidPoolTransaction<<Pool as TransactionPool>::Transaction>>>>
     where
         Pool: TransactionPool,
     {
-        if self.proposal.poll_tick(cx).is_ready() {
+        if self.proposal_interval.poll_tick(cx).is_ready() {
+            println!("Time going off");
             // drain the pool
             return Poll::Ready(pool.best_transactions().collect())
         }
