b8c836763f689321a6008fc038b797c2c28af38c
Armin Sabouri <armins88@gmail.com>
2023-11-03 13:26:13 -0400

    feature(authority): validate signer limit util

--- crates/consensus/authority/src/utils.rs
+++ crates/consensus/authority/src/utils.rs
@@ -24,13 +24,42 @@ pub(crate) fn read_staker_balance(
     let staking_contract_address = Address::from_slice(STAKING_CONTRACT_ADDRESS.as_bytes());
     let mut payload: Vec<Vec<u8>> = vec![];
     payload.push(staker_address.as_bytes().to_vec());
-    payload.push(H160::from_low_u64_le(STAKER_BALANCE_MAPPING_STORAGE_SLOT_INDEX).as_bytes().to_vec());
-    
+    payload
+        .push(H160::from_low_u64_le(STAKER_BALANCE_MAPPING_STORAGE_SLOT_INDEX).as_bytes().to_vec());
 
     let storage_key = keccak256(payload.into_iter().flatten().collect::<Vec<u8>>());
     let balance = provider
         .storage(staking_contract_address, storage_key)
-        .map_err(|_e| StorageAccessError::FailedAccess("Failed to retrieve storage"))?.unwrap();
+        .map_err(|_e| StorageAccessError::FailedAccess("Failed to retrieve storage"))?
+        .unwrap();
 
     Ok(balance)
+}
+
+pub(crate) enum RecoverAuthorityError {
+    NoSignaturePresentInExtraData,
+    FailedToRecoverSigner(secp256k1::Error),
+    FailedToDerserializeExtraData(botanix_lib::extra_data_header::ExtraDataHeaderDeserialzeError),
+    FailedToCreateSigHash(secp256k1::Error),
+}
+
+pub(crate) fn recovery_authority(
+    header: &Header,
+) -> Result<secp256k1::PublicKey, RecoverAuthorityError> {
+    let extra_data =
+        botanix_lib::extra_data_header::ExtraDataHeader::deserialize(header.extra_data.to_vec())
+            .map_err(|e| RecoverAuthorityError::FailedToDerserializeExtraData(e))?;
+
+    let sighash = create_authority_sighash(&mut header.clone(), &extra_data);
+    let message = secp256k1::Message::from_slice(sighash.as_bytes())
+        .map_err(|e| RecoverAuthorityError::FailedToCreateSigHash(e))?;
+
+    if let Some(signature) = extra_data.authority_signature {
+        let signer = signature
+            .recover(&message)
+            .map_err(|e| RecoverAuthorityError::FailedToRecoverSigner(e))?;
+        return Ok(signer)
+    }
+
+    Err(RecoverAuthorityError::NoSignaturePresentInExtraData)
 }
