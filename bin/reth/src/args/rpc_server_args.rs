db5479c35a0238c347adef48854d340b6e9df90b
Luca Provini <lucaprovini1989@gmail.com>
2023-11-11 15:54:32 +0100

    Feat/enable non namespaces (#5391)

--- bin/reth/src/args/rpc_server_args.rs
+++ bin/reth/src/args/rpc_server_args.rs
@@ -524,6 +524,7 @@ impl TypedValueParser for RpcModuleSelectionValueParser {
 mod tests {
     use super::*;
     use clap::Parser;
+    use reth_rpc_builder::RpcModuleSelection::Selection;
     use std::net::SocketAddrV4;
 
     /// A helper type to parse Args more easily
@@ -557,6 +558,14 @@ mod tests {
         let apis = args.http_api.unwrap();
         let expected = RpcModuleSelection::try_from_selection(["eth", "admin", "debug"]).unwrap();
 
+        assert_eq!(apis, expected);
+    }
+
+    #[test]
+    fn test_rpc_server_args_parser_none() {
+        let args = CommandParser::<RpcServerArgs>::parse_from(["reth", "--http.api", "none"]).args;
+        let apis = args.http_api.unwrap();
+        let expected = Selection(vec![]);
         assert_eq!(apis, expected);
     }
 
