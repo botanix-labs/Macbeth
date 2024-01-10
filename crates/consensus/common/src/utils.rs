f9285111bd325e11435478827a4d5f100e5aff16
Armin Sabouri <armins88@gmail.com>
2024-01-10 13:34:03 -0500

    feature(poa): util to validate current header against parent
    This change ensures that the same authority signer cannot sign
    multiple blocks in a row. Unless a sufficient amount of time has passed

--- crates/consensus/common/src/utils.rs
+++ crates/consensus/common/src/utils.rs
@@ -165,6 +165,59 @@ pub fn validate_poa_extra_data_header(
 
     Ok(())
 }
+
+/// Validate against parent header errors
+#[derive(Debug)]
+pub enum ValidateAgainstParrentError {
+    /// Signer limit exceeded
+    /// Could occur when signer is signings many blocks in the same turn
+    SignerLimitExceeded,
+    /// Failed to deserialize the extra data
+    FailedToDerserializeExtraData(RecoverAuthorityError),
+}
+
+impl From<ValidateAgainstParrentError> for ConsensusError {
+    fn from(e: ValidateAgainstParrentError) -> Self {
+        match e {
+            ValidateAgainstParrentError::SignerLimitExceeded => ConsensusError::SignerLimitExceeded,
+            ValidateAgainstParrentError::FailedToDerserializeExtraData(_) => {
+                ConsensusError::ExtraDataInvalid
+            }
+        }
+    }
+}
+
+pub fn validate_against_parent(
+    parent: Header,
+    current: Header,
+) -> Result<(), ValidateAgainstParrentError> {
+    // Gensis block does not have a federation signature, skip
+    if parent.number == 0 {
+        return Ok(())
+    }
+    let parent_signer = recovery_authority(&parent)
+        .map_err(|e| ValidateAgainstParrentError::FailedToDerserializeExtraData(e))?;
+    let current_signer = recovery_authority(&current)
+        .map_err(|e| ValidateAgainstParrentError::FailedToDerserializeExtraData(e))?;
+    let is_same_signer = parent_signer == current_signer;
+    // Check if the parent block was mined in a different turn
+    let parent_ts = parent.timestamp / 60;
+    let current_ts = current.timestamp / 60;
+
+    if !is_same_signer {
+        return Ok(());
+    }
+
+    // Last block should be greater that 1 minute in the worst cast
+    // Even in the case of > 2 federation members the worst case time between blocks for the same
+    // Signer should be 1 minute. Assuming 1 minute block times
+    if current_ts - parent_ts < 1 {
+        return Err(ValidateAgainstParrentError::SignerLimitExceeded)
+    }
+
+    Ok(())
+}
+
 mod tests {
     use std::str::FromStr;
 
@@ -173,6 +226,44 @@ mod tests {
 
     const EDH_DEFAULT_SIGHASH: &str =
         "0x0a088807360d347e57b95b64d765266f9551acc33ecfcdb2d49003a66acbf192";
+
+    const SK1: &str = "1aabc5cc52b62b570dc69001f1ab49cd1a7056bf6312fe058f094135f2c9b019";
+    const SK2: &str = "1bc1f5cc52b62b570dc69001f1ab49cd1a7056bf6312fe058f094135f2c9b019";
+
+    fn generate_secret_key(hex_string: &str) -> secp256k1::SecretKey {
+        secp256k1::SecretKey::from_str(hex_string).expect("Invalid hex string for SecretKey")
+    }
+
+    fn sign_block_helper(header: &mut Header, signer_sk: Option<&str>) -> () {
+        let mut edh = ExtraDataHeader::default();
+        let sk1 = generate_secret_key(SK1);
+        let sk2 = generate_secret_key(SK2);
+
+        edh.authority_signers = Some(vec![
+            secp256k1::PublicKey::from_secret_key(&secp256k1::Secp256k1::new(), &sk1),
+            secp256k1::PublicKey::from_secret_key(&secp256k1::Secp256k1::new(), &sk2),
+        ]);
+
+        let secp = secp256k1::Secp256k1::new();
+        header.number = 1;
+
+        let sighash = create_authority_sighash(&mut header.clone(), &edh);
+        let message = secp256k1::Message::from_slice(&sighash.as_slice()).unwrap();
+        let signature = {
+            if let Some(sk_str) = signer_sk {
+                let sk = generate_secret_key(sk_str);
+                secp256k1::Secp256k1::sign_ecdsa_recoverable(&secp, &message, &sk)
+            } else {
+                // By default sign with the first authority
+                secp256k1::Secp256k1::sign_ecdsa_recoverable(&secp, &message, &sk1)
+            }
+        };
+
+        edh.authority_signature = Some(signature);
+        edh.set_optional_fields_bitmask();
+
+        header.extra_data = Bytes::from(edh.serialize());
+    }
     /* Tests for create authority sighash utility */
     #[test]
     fn create_default_edh_sighhash() {
@@ -240,26 +331,9 @@ mod tests {
 
     #[test]
     fn should_recovery_authorities() {
-        let mut edh = ExtraDataHeader::default();
-        edh.authority_signers = Some(vec![
-            secp256k1::PublicKey::from_secret_key(
-                &secp256k1::Secp256k1::new(),
-                &secp256k1::SecretKey::from_str(
-                    "1aabc5cc52b62b570dc69001f1ab49cd1a7056bf6312fe058f094135f2c9b019",
-                )
-                .unwrap(),
-            ),
-            secp256k1::PublicKey::from_secret_key(
-                &secp256k1::Secp256k1::new(),
-                &secp256k1::SecretKey::from_str(
-                    "1bc1f5cc52b62b570dc69001f1ab49cd1a7056bf6312fe058f094135f2c9b019",
-                )
-                .unwrap(),
-            ),
-        ]);
-        edh.set_optional_fields_bitmask();
         let mut header = Header::default();
-        header.extra_data = Bytes::from(edh.serialize());
+        sign_block_helper(&mut header, None);
+        let edh = ExtraDataHeader::deserialize(&mut header.extra_data.to_vec().as_slice()).unwrap();
         let signer_list = get_authority_list(&header).unwrap();
 
         assert_eq!(signer_list, edh.authority_signers);
@@ -277,34 +351,12 @@ mod tests {
     // Tests for recover authority pk
     #[test]
     fn should_recover_authority() {
-        let mut edh = ExtraDataHeader::default();
-        let sk1 = secp256k1::SecretKey::from_str(
-            "1aabc5cc52b62b570dc69001f1ab49cd1a7056bf6312fe058f094135f2c9b019",
-        )
-        .unwrap();
-        let sk2 = secp256k1::SecretKey::from_str(
-            "1bc1f5cc52b62b570dc69001f1ab49cd1a7056bf6312fe058f094135f2c9b019",
-        )
-        .unwrap();
-
-        edh.authority_signers = Some(vec![
-            secp256k1::PublicKey::from_secret_key(&secp256k1::Secp256k1::new(), &sk1),
-            secp256k1::PublicKey::from_secret_key(&secp256k1::Secp256k1::new(), &sk2),
-        ]);
-
         let mut header = Header::default();
+        sign_block_helper(&mut header, None);
+        let edh = ExtraDataHeader::deserialize(&mut header.extra_data.to_vec().as_slice()).unwrap();
 
-        let sighash = create_authority_sighash(&mut header, &edh);
-        let secp = secp256k1::Secp256k1::new();
-        let message = secp256k1::Message::from_slice(&sighash.as_slice()).unwrap();
-        let signature = secp256k1::Secp256k1::sign_ecdsa_recoverable(&secp, &message, &sk1);
-
-        edh.authority_signature = Some(signature);
-        edh.set_optional_fields_bitmask();
-
-        header.extra_data = Bytes::from(edh.serialize());
         let recovered = recovery_authority(&header).unwrap();
-
+        // utility function above only signs with the first authority
         assert_eq!(recovered, edh.authority_signers.unwrap()[0]);
     }
 
@@ -353,35 +405,11 @@ mod tests {
     }
 
     #[test]
-    fn should_validate() {
+    fn should_validate_poa_header() {
         // In this case we are signing with a non federation different key
-        let mut edh = ExtraDataHeader::default();
-        let sk1 = secp256k1::SecretKey::from_str(
-            "1aabc5cc52b62b570dc69001f1ab49cd1a7056bf6312fe058f094135f2c9b019",
-        )
-        .unwrap();
-        let sk2 = secp256k1::SecretKey::from_str(
-            "1bc1f5cc52b62b570dc69001f1ab49cd1a7056bf6312fe058f094135f2c9b019",
-        )
-        .unwrap();
-
-        edh.authority_signers = Some(vec![
-            secp256k1::PublicKey::from_secret_key(&secp256k1::Secp256k1::new(), &sk1),
-            secp256k1::PublicKey::from_secret_key(&secp256k1::Secp256k1::new(), &sk2),
-        ]);
-
-        let secp = secp256k1::Secp256k1::new();
         let mut header = Header::default();
-        header.number = 1;
-
-        let sighash = create_authority_sighash(&mut header, &edh);
-        let message = secp256k1::Message::from_slice(&sighash.as_slice()).unwrap();
-        let signature = secp256k1::Secp256k1::sign_ecdsa_recoverable(&secp, &message, &sk1);
-
-        edh.authority_signature = Some(signature);
-        edh.set_optional_fields_bitmask();
-
-        header.extra_data = Bytes::from(edh.serialize());
+        sign_block_helper(&mut header, None);
+        let edh = ExtraDataHeader::deserialize(&mut header.extra_data.to_vec().as_slice()).unwrap();
         let authority_signers = edh.authority_signers.unwrap();
         let result = validate_poa_extra_data_header(&header, &authority_signers);
         assert!(result.is_ok());
@@ -428,5 +456,61 @@ mod tests {
         let beneficiary_address = public_key_to_address(auth_signer2);
         header.beneficiary = beneficiary_address;
         assert!(validate_poa_block_beneficiary(&header, &authority_signers).is_ok());
+
+    }
+
+    #[test]
+    fn validate_against_parent_skip_gensis() {
+        let mut parent = Header::default();
+        parent.number = 0;
+        let current = Header::default();
+        let result = validate_against_parent(parent, current);
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    fn should_fail_with_same_signer() {
+        let mut parent = Header::default();
+        let mut current = Header::default();
+
+        parent.number = 1;
+        current.number = 2;
+
+        sign_block_helper(&mut parent, None);
+        sign_block_helper(&mut current, None);
+
+        let result = validate_against_parent(parent, current);
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn should_pass_after_sufficient_time() {
+        let mut parent = Header::default();
+        let mut current = Header::default();
+
+        parent.number = 1;
+        parent.timestamp = 1704834442 as u64;
+        current.number = 2;
+        current.timestamp = 1704834442 as u64 + 60;
+
+        sign_block_helper(&mut parent, None);
+        sign_block_helper(&mut current, None);
+
+        let result = validate_against_parent(parent, current);
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    fn should_pass_with_different_signer() {
+        let mut parent = Header::default();
+        let mut current = Header::default();
+        parent.number = 1;
+        current.number = 2;
+
+        sign_block_helper(&mut parent, None);
+        sign_block_helper(&mut current, Some(SK2));
+
+        let result = validate_against_parent(parent, current);
+        assert!(result.is_ok());
     }
 }
