fb973474bac8b89baf47ae21de01f80254c0a903
Steven Roose <steven@stevenroose.org>
2024-04-25 16:55:18 +0100

    nix-shell: Update to Rust version 1.77.2

--- shell.nix
+++ shell.nix
@@ -1,14 +1,27 @@
-{ pkgs ? import <nixpkgs> {} }:
 
+let
+  rustOverlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
+  pkgs = import <nixpkgs> { overlays = [ rustOverlay ]; };
+  #rustVersion = "latest";
+  rustVersion = "1.77.2";
+  rust = pkgs.rust-bin.stable.${rustVersion}.default.override {
+    extensions = [ "rust-analyzer" "rust-src" ];
+  };
+in
 pkgs.mkShell {
-  buildInputs = with pkgs; [
+  buildInputs = [ rust ] ++ (with pkgs; [
     pkg-config
     openssl
     glibc
     clang
     libclang
     protobuf
-  ];
+    rustc
+    cargo
+  ]);
 
+  # PROJECT_ROOT = builtins.toString ./.;
+  RUST_BACKTRACE = 1;
+  RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
   LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
 }
