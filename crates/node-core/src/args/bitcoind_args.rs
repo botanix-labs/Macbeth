121f0e1eeff218c2a7417d6ae4c6acb6593f1d20
Armin Sabouri <armins88@gmail.com>
2024-04-26 23:13:33 -0400

    fix: missing default values on rpc args

--- crates/node-core/src/args/bitcoind_args.rs
+++ crates/node-core/src/args/bitcoind_args.rs
@@ -2,6 +2,17 @@ use clap::Args;
 use reth_btc_wallet::bitcoind::BitcoindConfig;
 use url::Url;
 
+use super::utils::parse_url;
+
+/// Default bitcoind url
+pub(crate) const DEFAULT_BITCOIND_URL: &str = "localhost:18443";
+
+/// Default bitcoind username
+pub(crate) const DEFAULT_BITCOIND_USERNAME: &str = "foo";
+
+/// Default bitcoind password
+pub(crate) const DEFAULT_BITCOIND_PASSWORD: &str = "bar";
+
 /// Parameters to configure Bitcoind.
 #[derive(Debug, Clone, Args, PartialEq, Eq)]
 #[clap(next_help_heading = "Bitcoind")]
@@ -10,13 +21,14 @@ pub struct BitcoindArgs {
     /// bitcoind RPC url
     ///
     /// The url of the bitcoind server.
-    #[arg(long = "bitcoind.url", name = "bitcoind.url", value_name = "BITCOIND_URL")]
+    #[arg(default_value_t=Url::parse(DEFAULT_BITCOIND_URL).expect("valid url"), value_parser = parse_url, long = "bitcoind.url", name = "bitcoind.url", value_name = "BITCOIND_URL")]
     pub url: Url,
 
     /// Btcd username
     ///
     /// The username of the bitcoind server.
     #[arg(
+        default_value_t = DEFAULT_BITCOIND_USERNAME.into(),
         long = "bitcoind.username",
         name = "bitcoind.username",
         value_name = "BITCOIND_USERNAME"
@@ -27,6 +39,7 @@ pub struct BitcoindArgs {
     ///
     /// The password of the bitcoind server.
     #[arg(
+        default_value_t = DEFAULT_BITCOIND_PASSWORD.into(),
         long = "bitcoind.password",
         name = "bitcoind.password",
         value_name = "BITCOIND_PASSWORD"
