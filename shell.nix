19235a73dbcd37712fab6da4b3c9703ad33511c3
Steven Roose <steven@stevenroose.org>
2024-05-21 15:35:40 +0100

    nix-shell: Use rustup to fix versions

--- shell.nix
+++ shell.nix
@@ -1,23 +1,26 @@
+{ pkgs ? import <nixpkgs> {} }:
 
 let
-  rustOverlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
-  pkgs = import <nixpkgs> { overlays = [ rustOverlay ]; };
-  #rustVersion = "latest";
-  rustVersion = "1.77.2";
-  rust = pkgs.rust-bin.stable.${rustVersion}.default.override {
-    extensions = [ "rust-analyzer" "rust-src" ];
-  };
+  # this code allows setting an version fix overlay for Rust
+  # rustOverlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
+  # pkgs = import <nixpkgs> { overlays = [ rustOverlay ]; };
+  # #rustVersion = "latest";
+  # rustVersion = "1.77.2";
+  # rust = pkgs.rust-bin.stable.${rustVersion}.default.override {
+  #   extensions = [ "rust-analyzer" "rust-src" ];
+  # };
+
   protobuf = pkgs.protobuf3_23;
 in
 pkgs.mkShell {
-  buildInputs = [ rust protobuf ] ++ (with pkgs; [
+  # buildInputs = [ rust protobuf ] ++ (with pkgs; [
+  buildInputs = [ protobuf ] ++ (with pkgs; [
     pkg-config
     openssl
     glibc
     clang
     libclang
-    rustc
-    cargo
+    rustup
   ]);
 
   # PROJECT_ROOT = builtins.toString ./.;
@@ -25,4 +28,5 @@ pkgs.mkShell {
   RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
   LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
   PROTOC = "${protobuf}/bin/protoc";
+  RUSTUP_TOOLCHAIN = "1.77.2";
 }
