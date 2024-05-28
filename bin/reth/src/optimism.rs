c92f3e973319e8b5a3681b8653819f79076c5da6
Armin Sabouri <armins88@gmail.com>
2024-05-28 12:18:36 -0400

    panic in optimism

--- bin/reth/src/optimism.rs
+++ bin/reth/src/optimism.rs
@@ -26,47 +26,49 @@ fn main() {
         std::env::set_var("RUST_BACKTRACE", "1");
     }
 
-    if let Err(err) = Cli::<RollupArgs>::parse().run(|builder, rollup_args| async move {
-        let NodeHandle { node, node_exit_future } = builder
-            .node(OptimismNode::new(rollup_args.clone()))
-            .extend_rpc_modules(move |ctx| {
-                // register sequencer tx forwarder
-                if let Some(sequencer_http) = rollup_args.sequencer_http {
-                    ctx.registry.set_eth_raw_transaction_forwarder(Arc::new(SequencerClient::new(
-                        sequencer_http,
-                    )));
-                }
+    panic!("Optimism is not yet supported in this version of reth");
 
-                Ok(())
-            })
-            .launch()
-            .await?;
+    // if let Err(err) = Cli::<RollupArgs>::parse().run(|builder, rollup_args| async move {
+    //     let NodeHandle { node, node_exit_future } = builder
+    //         .node(OptimismNode::new(rollup_args.clone()))
+    //         .extend_rpc_modules(move |ctx| {
+    //             // register sequencer tx forwarder
+    //             if let Some(sequencer_http) = rollup_args.sequencer_http {
+    //                 ctx.registry.set_eth_raw_transaction_forwarder(Arc::new(SequencerClient::new(
+    //                     sequencer_http,
+    //                 )));
+    //             }
 
-        // If `enable_genesis_walkback` is set to true, the rollup client will need to
-        // perform the derivation pipeline from genesis, validating the data dir.
-        // When set to false, set the finalized, safe, and unsafe head block hashes
-        // on the rollup client using a fork choice update. This prevents the rollup
-        // client from performing the derivation pipeline from genesis, and instead
-        // starts syncing from the current tip in the DB.
-        if node.chain_spec().is_optimism() && !rollup_args.enable_genesis_walkback {
-            let client = node.rpc_server_handles.auth.http_client();
-            if let Ok(Some(head)) = node.provider.latest_header() {
-                reth_rpc_api::EngineApiClient::<OptimismEngineTypes>::fork_choice_updated_v2(
-                    &client,
-                    reth_rpc_types::engine::ForkchoiceState {
-                        head_block_hash: head.hash(),
-                        safe_block_hash: head.hash(),
-                        finalized_block_hash: head.hash(),
-                    },
-                    None,
-                )
-                .await?;
-            }
-        }
+    //             Ok(())
+    //         })
+    //         .launch()
+    //         .await?;
 
-        node_exit_future.await
-    }) {
-        eprintln!("Error: {err:?}");
-        std::process::exit(1);
-    }
+    //     // If `enable_genesis_walkback` is set to true, the rollup client will need to
+    //     // perform the derivation pipeline from genesis, validating the data dir.
+    //     // When set to false, set the finalized, safe, and unsafe head block hashes
+    //     // on the rollup client using a fork choice update. This prevents the rollup
+    //     // client from performing the derivation pipeline from genesis, and instead
+    //     // starts syncing from the current tip in the DB.
+    //     if node.chain_spec().is_optimism() && !rollup_args.enable_genesis_walkback {
+    //         let client = node.rpc_server_handles.auth.http_client();
+    //         if let Ok(Some(head)) = node.provider.latest_header() {
+    //             reth_rpc_api::EngineApiClient::<OptimismEngineTypes>::fork_choice_updated_v2(
+    //                 &client,
+    //                 reth_rpc_types::engine::ForkchoiceState {
+    //                     head_block_hash: head.hash(),
+    //                     safe_block_hash: head.hash(),
+    //                     finalized_block_hash: head.hash(),
+    //                 },
+    //                 None,
+    //             )
+    //             .await?;
+    //         }
+    //     }
+
+    //     node_exit_future.await
+    // }) {
+    //     eprintln!("Error: {err:?}");
+    //     std::process::exit(1);
+    // }
 }
