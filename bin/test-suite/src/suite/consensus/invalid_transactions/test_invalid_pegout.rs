5aa8dac748a334bc1c9c96fb956c6cfe606e4a93
Scott Millner <22253830+scottmillner@users.noreply.github.com>
2024-06-13 13:51:48 -0600

    fix: update state for pegins/pegouts, account for all fail paths

--- bin/test-suite/src/suite/consensus/invalid_transactions/test_invalid_pegout.rs
+++ bin/test-suite/src/suite/consensus/invalid_transactions/test_invalid_pegout.rs
@@ -1,5 +1,9 @@
 use bitcoin::Amount;
 use reth_botanix_lib::utils::AmountExt;
+use reth_btc_wallet::address::EthAddress;
+use reth_cli_runner::CliRunner;
+use reth_primitives::Address;
+use std::time::Duration;
 
 use crate::{it_info_print, suite::consensus::ConsensusIntegrationTestSuite};
 
@@ -19,9 +23,22 @@ pub async fn invalid_pegout(
     let invalid_pegout_destination = ethers::core::types::Bytes::from(
         "invalid_pegout_destination".to_string().as_bytes().to_vec(),
     );
+
+    let sender_address = botanix_eth_client.get_sender_address();
+    let sender_address_string = Address::from_slice(sender_address.as_slice()).to_string();
+    // sender address balance before pegout
+    let mut sender_address_initial_balance =
+        botanix_eth_client.get_botanix_balance(sender_address_string.as_str()).await.unwrap();
+    it_info_print!("Sender address initial balance: ", sender_address_initial_balance);
+
+    // nonce before pegout
+    let nonce_before = botanix_eth_client.get_nonce(sender_address.clone()).await.unwrap();
+    it_info_print!("Nonce before pegout: ", nonce_before);
+
     // use empty pegout data
     let pegout_data = ethers::core::types::Bytes::new();
     let pegout_amount = Amount::from_btc(0.5).unwrap();
+    it_info_print!("Pegout amount: ", pegout_amount.to_wei());
     let tx_receipt = botanix_eth_client
         .burn(invalid_pegout_destination, pegout_data, pegout_amount.to_wei())
         .await
@@ -30,6 +47,24 @@ pub async fn invalid_pegout(
     it_info_print!("Pegout Tx Receipt: ", tx_receipt);
 
     assert!(tx_receipt.status.unwrap().is_zero());
+
+    // sender address balance after pegout
+    let sender_address_final_balance =
+        botanix_eth_client.get_botanix_balance(sender_address_string.as_str()).await.unwrap();
+    it_info_print!("Sender address final balance: ", sender_address_final_balance);
+
+    // subtract tx costs from initial balance
+    let tx_cost = tx_receipt.gas_used.unwrap() * tx_receipt.effective_gas_price.unwrap();
+    it_info_print!("Tx cost: ", tx_cost);
+    sender_address_initial_balance -= tx_cost;
+
+    assert_eq!(sender_address_initial_balance, sender_address_final_balance);
+
+    // nonce after pegout
+    let nonce_after = botanix_eth_client.get_nonce(sender_address.clone()).await.unwrap();
+    it_info_print!("Nonce after pegout: ", nonce_after);
+
+    assert!(nonce_after > nonce_before);
 
     Ok(())
 }
