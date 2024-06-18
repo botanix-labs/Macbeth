e1e14da38d726a2d13530be19790c9112fec0f0b
Scott Millner <22253830+scottmillner@users.noreply.github.com>
2024-06-18 17:18:01 -0600

    refactor: add validate_extra_data_header_authorities() and test

--- crates/consensus/authority/src/lib.rs
+++ crates/consensus/authority/src/lib.rs
@@ -21,7 +21,7 @@
 //! be mined.
 use reth_consensus::{Consensus, ConsensusError};
 use reth_consensus_common::{
-    utils::{get_block_producer_address, unix_timestamp},
+    utils::{get_block_producer_address, unix_timestamp, validate_extra_data_header_authorities},
     validation::{self, validate_poa_header_standalone, validate_poa_header_template_standalone},
 };
 use reth_interfaces::{
@@ -52,7 +52,7 @@ use std::sync::Arc;
 
 use tokio::sync::{RwLock, RwLockReadGuard, RwLockWriteGuard};
 
-use tracing::{trace, warn};
+use tracing::{error, trace, warn};
 mod block_builder;
 mod block_fetcher;
 mod builder;
@@ -187,7 +187,9 @@ pub(crate) struct StorageInner<Client> {
     client: Client,
     /// The authority list in the genesis block
     pub(crate) genesis_authorities: Vec<secp256k1::PublicKey>,
-    /// Keep track of the  signers
+    /// Keep track of the signers
+    /// This value is pulled from the latest epoch block EDH
+    /// and should be the same as genesis_authorities as long as the federation is static
     pub(crate) authorities: Vec<secp256k1::PublicKey>,
     /// keep track of my place among the signer
     /// This will change as new signers are removed
@@ -482,9 +484,17 @@ where
             utxo_commitment,
         )?;
 
+        // Validate EDH authorities match genesis authorities
+        if let Err(e) = validate_extra_data_header_authorities(&header, &self.genesis_authorities) {
+            error!(target: "consensus::authority", "failed to validate EDH authorities: {:?}", e);
+            return Err(BlockExecutionError::Validation(
+                BlockValidationError::InvalidExtraDataAuthorities,
+            ));
+        }
+
         // Redundant check. Lets make sure the header is valid
         validate_poa_header_template_standalone(&header, authority_signers).map_err(|e| {
-            warn!(target: "consensus::authority", "failed to validate POA header: {:?}", e);
+            error!(target: "consensus::authority", "failed to validate POA header: {:?}", e);
             // TODO(armins) return more expressive error
             BlockExecutionError::Validation(BlockValidationError::InvalidExtraData)
         })?;
