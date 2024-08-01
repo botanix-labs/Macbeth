d9c931c74e68b3d841351bb0fc9fbccbd2d20ba2
Armin Sabouri <armins88@gmail.com>
2024-08-01 15:18:51 -0400

    test: use correct pegin confirmation depth

--- bin/test-suite/src/suite/consensus/frost/test_frost_e2e.rs
+++ bin/test-suite/src/suite/consensus/frost/test_frost_e2e.rs
@@ -14,7 +14,7 @@ use reth_botanix_lib::{
 };
 use reth_btc_wallet::address::EthAddress;
 
-use reth_primitives::Address;
+use reth_primitives::{Address, BOTANIX_TESTNET};
 
 use crate::{
     it_info_print,
@@ -30,7 +30,8 @@ use crate::{
 pub async fn frost_e2e_stable(
     suite: &ConsensusIntegrationTestSuite,
 ) -> Result<(), super::error::Error> {
-    let pegin_conf_depth = 6; //TODO(stevenroose) set this from chain constant?
+    let pegin_conf_depth = BOTANIX_TESTNET.parent_confirmation_depth;
+    it_info_print!("Pegin Confirmation Depth", pegin_conf_depth);
 
     // Set up regtest connection
     // config is hardcoded to only work with regtest
@@ -102,7 +103,9 @@ pub async fn frost_e2e_stable(
         .send_to_address(&btc_address, Amount::ONE_BTC, None, None, Some(true), None, Some(1), None)
         .expect("valid send");
     // Generate some block to confirm it
-    bitcoind_rpc.generate_to_address(2 + pegin_conf_depth, &address).expect("generate to address");
+    bitcoind_rpc
+        .generate_to_address(2 + pegin_conf_depth as u64, &address)
+        .expect("generate to address");
     tokio::time::sleep(Duration::from_secs(5)).await;
 
     // retrieve the transaction
@@ -178,7 +181,7 @@ pub async fn frost_e2e_stable(
     };
     let finalized = {
         let tip = bitcoind_rpc.get_block_count().unwrap();
-        let height = tip - pegin_conf_depth;
+        let height = tip - (pegin_conf_depth - 1) as u64;
         let hash = bitcoind_rpc.get_block_hash(height).unwrap();
         (bitcoind_rpc.get_block_header(&hash).unwrap(), height as u32)
     };
