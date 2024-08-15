0101960a9c2dc519e0c97bb56c63d7069ffe15bf
Scott Millner <22253830+scottmillner@users.noreply.github.com>
2024-08-14 18:35:47 -0600

    test: refactor test but don't include

--- bin/test-suite/src/suite/consensus/frost/test_edh_size_limit.rs
+++ bin/test-suite/src/suite/consensus/frost/test_edh_size_limit.rs
@@ -10,6 +10,7 @@ use crate::{
 };
 use bitcoin::{hashes::Hash, merkle_tree::PartialMerkleTree, Amount};
 use bitcoincore_rpc::RpcApi;
+use core::panic;
 use ethers::{
     prelude::Provider,
     providers::{Http, PendingTransaction},
@@ -25,6 +26,12 @@ use reth_primitives::{Address, BOTANIX_TESTNET};
 use std::{str::FromStr, time::Duration};
 
 const NUM_PEGINS: u16 = 1000;
+
+// NOTE: this test cannot run as currently implemented
+// we need to send 1000 pegins and each pegin is in a different block
+// so the headers list is huge for the earlier pegins
+// and quickly fills a block to it's limit of 30 million gas
+// and to wait for all blocks to be mined is way too long
 
 pub async fn test_edh_size_limit(suite: &ConsensusIntegrationTestSuite) -> Result<(), Error> {
     let pegin_conf_depth = BOTANIX_TESTNET.parent_confirmation_depth;
@@ -43,11 +50,12 @@ pub async fn test_edh_size_limit(suite: &ConsensusIntegrationTestSuite) -> Resul
     }
     let address =
         bitcoind_rpc.get_new_address(None, None).expect("get new address").assume_checked();
-    bitcoind_rpc.generate_to_address((NUM_PEGINS).into(), &address).expect("generate to address");
+    // generate > 100 blocks so coinbase utxos can be spent from the wallet
+    bitcoind_rpc.generate_to_address((101), &address).expect("generate to address");
     tokio::time::sleep(Duration::from_secs(5)).await;
 
     // Print wallet balancee
-    let balance = bitcoind_rpc.get_balance(None, None).expect("get balance");
+    let mut balance = bitcoind_rpc.get_balance(None, None).expect("get balance");
     it_info_print!("Wallet Balance", balance);
 
     let test_fed_members = suite
@@ -75,23 +83,22 @@ pub async fn test_edh_size_limit(suite: &ConsensusIntegrationTestSuite) -> Resul
 
     // Set up dummy eth address
     let mut pegin_txsids = Vec::new();
-    for index in 0..NUM_PEGINS {
+    let eth_destination = ethers::core::types::Address::random();
+    // get gateway address
+    let gateway_address_response = provider
+        .request::<Vec<String>, GatewayAddressResponse>(
+            "eth_getGatewayAddress",
+            vec![hex::encode(eth_destination.0)],
+        )
+        .await
+        .expect("should get gateway address");
+    let btc_address = bitcoin::Address::from_str(gateway_address_response.gateway_address.as_str())
+        .expect("valid btc_address")
+        .assume_checked();
+    for index in 0..NUM_PEGINS - 1 {
         it_info_print!("Pegin #", index);
-        let eth_destination = ethers::core::types::Address::random();
-        // get gateway address
-        let gateway_address_response = provider
-            .request::<Vec<String>, GatewayAddressResponse>(
-                "eth_getGatewayAddress",
-                vec![hex::encode(eth_destination.0)],
-            )
-            .await
-            .expect("should get gateway address");
+        it_info_print!("Pegin amount", Amount::from_sat(balance.to_sat() / NUM_PEGINS as u64));
 
-        // Send some bitcoin to that gateway address
-        let btc_address =
-            bitcoin::Address::from_str(gateway_address_response.gateway_address.as_str())
-                .expect("valid btc_address")
-                .assume_checked();
         let pegin_txid = bitcoind_rpc
             .send_to_address(
                 &btc_address,
@@ -108,8 +115,16 @@ pub async fn test_edh_size_limit(suite: &ConsensusIntegrationTestSuite) -> Resul
             gateway_address_response.aggregate_public_key.as_str(),
         )
         .expect("valid agg pk");
+        let blocks = bitcoind_rpc.generate_to_address((1), &address).expect("generate to address");
 
-        pegin_txsids.push((pegin_txid, eth_destination, btc_address, agg_pk));
+        pegin_txsids.push((
+            pegin_txid,
+            eth_destination,
+            btc_address.clone(),
+            agg_pk,
+            blocks[0],
+            vec![],
+        ));
     }
 
     // Generate some block to confirm all pegins
@@ -118,41 +133,41 @@ pub async fn test_edh_size_limit(suite: &ConsensusIntegrationTestSuite) -> Resul
         .expect("generate to address");
     tokio::time::sleep(Duration::from_secs(5)).await;
 
-    // All the pegins should be in the same block by now
     // Lets assemble the headers we need for the proof
+    // We will collect all the headers all the way up to the tip which is not needed, but allowed.
+    // In theory, we only need to collect headers from the block our pegin is in, to the finalized
+    // block (the one in the mainchain commitment).
     let tip = bitcoind_rpc.get_best_block_hash().unwrap();
     it_info_print!("Bitcoin Chain Tip", tip);
     let tip_header = bitcoind_rpc.get_block_header(&tip).expect("valid block header");
 
-    // Again assuming all pegin txs are in the same block
-    let conf_hash = bitcoind_rpc
-        .get_transaction(&pegin_txsids[0].0, None)
-        .expect("valid txid")
-        .info
-        .blockhash
-        .expect("pegin confirmed");
+    for index in 0..NUM_PEGINS - 1 {
+        let i = index as usize;
+        // Get confirmation block hash for each pegin and build headers list
+        let conf_hash = &pegin_txsids[i].4;
+        let mut headers = vec![];
+        let mut cursor = tip_header;
+        let mut stopgap = 2000; // just to make sure we don't infinite loop until genesis
+        loop {
+            stopgap -= 1;
+            if stopgap == 0 || cursor.prev_blockhash == bitcoin::BlockHash::all_zeros() {
+                panic!("confirmation block not found...");
+            }
 
-    let mut headers = vec![];
-    let mut cursor = tip_header;
-    let mut stopgap = NUM_PEGINS + 1; // just to make sure we don't infinite loop until genesis
-    loop {
-        stopgap -= 1;
-        if stopgap == 0 || cursor.prev_blockhash == bitcoin::BlockHash::all_zeros() {
-            panic!("confirmation block not found...");
+            headers.push(cursor);
+            if cursor.block_hash() == *conf_hash {
+                break;
+            }
+            cursor = bitcoind_rpc.get_block_header(&cursor.prev_blockhash).unwrap();
         }
+        headers.reverse();
 
-        headers.push(cursor);
-        if cursor.block_hash() == conf_hash {
-            break;
-        }
-        cursor = bitcoind_rpc.get_block_header(&cursor.prev_blockhash).unwrap();
+        // add headers to pegin
+        pegin_txsids[i].5 = headers.clone();
+        it_info_print!("Pegin #", index);
+        it_info_print!("Number of pegin_headers:", headers.len());
     }
-    headers.reverse();
-    it_info_print!("Number of pegin_headers:", headers.len());
 
-    // We will collect all the headers all the way up to the tip which is not needed, but allowed.
-    // In theory, we only need to collect headers from the block our pegin is in, to the finalized
-    // block (the one in the mainchain commitment).
     let checkpoint = {
         let tip = bitcoind_rpc.get_block_count().unwrap();
         let height = tip - pegin_conf_depth as u64;
@@ -161,7 +176,7 @@ pub async fn test_edh_size_limit(suite: &ConsensusIntegrationTestSuite) -> Resul
     };
 
     let mut pegins = vec![];
-    for (txid, eth_address, btc_address, agg_pk) in pegin_txsids {
+    for (txid, eth_address, btc_address, agg_pk, _block, headers) in pegin_txsids {
         // retrieve the transaction
         let tx_res = bitcoind_rpc.get_transaction(&txid, None).expect("valid tx");
         assert!(tx_res.info.confirmations > 1);
@@ -211,15 +226,41 @@ pub async fn test_edh_size_limit(suite: &ConsensusIntegrationTestSuite) -> Resul
         pegins.push(pegin_data);
     }
 
-    // mint all the pegins
+    // mint all the pegins across multiple blocks
     let refund_address = ethers::core::types::Address::random();
     let mut tx_hashes = vec![];
     let provider = test_fed_members.get(&0).unwrap().create_botanix_eth_client().await;
     let mut nonce = provider.nonce().await;
-    for (_, pegin) in pegins.iter().enumerate() {
+    for (index, pegin) in pegins.iter().enumerate() {
+        it_info_print!("Pegin #", index);
+
         // There is only one pegin that needs to be serialized
         let serialized_pegin_meta = pegin.meta[0].serialize();
         let metadata = ethers::core::types::Bytes::from(serialized_pegin_meta.clone());
+
+        // check of block if full (max gas limit = 30 million)
+        let block = provider.get_pending_block().await.expect("block to exist");
+        // if gas used > 28 million, then we need to wait for the next block
+        it_info_print!("Block Gas Used", block.gas_used);
+        if block.gas_used.as_u64() > 28_000_000_u64 {
+            it_info_print!("Block is full, waiting for next block");
+
+            it_info_print!("Waiting for current Pegins to be mined!");
+            let http_provider = provider.provider().clone();
+            for (index, tx_hash) in tx_hashes.clone().iter().enumerate() {
+                let pending_tx = PendingTransaction::new(
+                    ethers::core::types::H256::from(tx_hash),
+                    &http_provider,
+                );
+
+                pending_tx.await.expect("tx should be mined");
+                it_info_print!("Pegin mined!", tx_hash);
+
+                // remove the tx from the list
+                tx_hashes.remove(index);
+            }
+        }
+
         let tx_hash = provider
             .non_confirmed_mint(
                 ethers::core::types::Address::from_slice(pegin.account.as_slice()),
@@ -236,50 +277,18 @@ pub async fn test_edh_size_limit(suite: &ConsensusIntegrationTestSuite) -> Resul
         tx_hashes.push(tx_hash);
     }
 
-    it_info_print!("Waiting for all Pegins to be mined!");
-    let http_provider = provider.provider().clone();
-    for tx_hash in tx_hashes {
-        let pending_tx =
-            PendingTransaction::new(ethers::core::types::H256::from(&tx_hash), &http_provider);
-
-        pending_tx.await.expect("tx should be mined");
-        it_info_print!("Pegin mined!", tx_hash);
-    }
-
     it_info_print!("Minted all the pegins");
-    it_info_print!("Waiting for botanix event after mint call");
-    // There should be a mint event for each pegin
-    for _ in 0..NUM_PEGINS {
-        await_botanix_event(&mut rx, *MINT_TOPIC).await;
-    }
     tokio::time::sleep(Duration::from_secs(5)).await;
-    // Ensure each eth address has a non zero balance
-    for (_, pegin) in pegins.iter().enumerate() {
-        let eth_address_balance =
-            provider.get_botanix_balance(pegin.account.to_string().as_str()).await;
-        assert!(!eth_address_balance.expect("get balance").is_zero());
-    }
+    // Ensure eth address has a non zero balance
+    let eth_address_balance = provider
+        .get_botanix_balance(eth_destination.to_string().as_str())
+        .await
+        .expect("balance to exist");
+    assert!(!eth_address_balance.is_zero());
 
     // Check refund address has a non zero balance
     let refund_address_balance = provider.get_balance(refund_address).await;
     assert!(!refund_address_balance.expect("get balance").is_zero());
-
-    let utxos = suite.local_context.btc_server_clients.clone().expect("btc server clients")[0]
-        .get_all_utxos(client::Empty {})
-        .await
-        .unwrap()
-        .into_inner()
-        .utxos;
-    it_info_print!("UTXOs", utxos);
-
-    for pegin in pegins.iter() {
-        let utxo = utxos.iter().find(|utxo| {
-            bitcoin::Txid::from_slice(utxo.outpoint.as_ref().unwrap().txid.as_slice())
-                .expect("valid txid")
-                == pegin.meta[0].tx.txid()
-        });
-        assert!(utxo.is_some());
-    }
 
     // send a pegout which should be successful bc inputs <= MAX_EDH_SIZE
     // Generate and send pegout tx: arbitrarily choose the first btc address
@@ -287,10 +296,9 @@ pub async fn test_edh_size_limit(suite: &ConsensusIntegrationTestSuite) -> Resul
         ethers::core::types::Bytes::from(address.to_string().as_bytes().to_vec());
     // use empty pegout data
     let pegout_data = ethers::core::types::Bytes::new();
-    // each input is 1 sat for a total of 1000 sats
-    let pegout_amount = balance;
+    let pegout_amount = eth_address_balance;
     let pegout_tx_receipt =
-        provider.burn(pegout_destination, pegout_data, pegout_amount.to_wei()).await.unwrap();
+        provider.burn(pegout_destination, pegout_data, pegout_amount).await.unwrap();
     it_info_print!("Pegout Tx Receipt: ", pegout_tx_receipt);
 
     // wait for the tx to be included in a botanix block
