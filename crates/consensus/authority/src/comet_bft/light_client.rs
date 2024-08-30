85d1e85f3d0163921ac0d35e256b2fc6058e27f2
Armin Sabouri <armins88@gmail.com>
2024-08-30 12:07:54 -0400

    fixup! feature(abci): allow light store to be configurable

--- crates/consensus/authority/src/comet_bft/light_client.rs
+++ crates/consensus/authority/src/comet_bft/light_client.rs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 use std::time::Duration;
 
 use comet_bft_rpc::{Client, CometBftRpcFactory, HttpCometBFTRpcClientFactory};
@@ -7,6 +8,14 @@ use tendermint_light_client::{
     light_client::{LightClient, Options},
     store::{memory::MemoryStore, LightStore},
     types::{Height, TrustThreshold},
+=======
+use comet_bft_rpc::{CometBftRpcFactory, HttpCometBFTRpcClientFactory};
+use tendermint_light_client::{
+    builder::LightClientBuilder,
+    instance::Instance,
+    light_client::LightClient,
+    store::{memory::MemoryStore, LightStore},
+>>>>>>> 0aaacbc35 (fixup! feature(abci): allow light store to be configurable)
 };
 
 /// Builds a light client from a comet bft rpc client factory and a light store
@@ -30,9 +39,13 @@ impl LightCBFTClientBuilder {
         self
     }
 
+<<<<<<< HEAD
     pub async fn build_and_verify(&self) -> Instance {
         let light_store = Box::new(MemoryStore::new());
 
+=======
+    pub async fn build(self) -> Instance {
+>>>>>>> 0aaacbc35 (fixup! feature(abci): allow light store to be configurable)
         let rpc_client =
             self.rpc_client_factory.build_and_connect().expect("should connect to RPC client");
         let node_id = rpc_client.status().await.expect("Failed to get node info").node_info.id;
