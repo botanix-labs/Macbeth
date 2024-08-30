d7eb295c6620349fc12e63ad5efb2b43a2a496a3
Armin Sabouri <armins88@gmail.com>
2024-08-30 08:34:34 -0400

    feature(abci): allow light store to be configurable

--- crates/consensus/authority/src/comet_bft/light_client.rs
+++ crates/consensus/authority/src/comet_bft/light_client.rs
@@ -1,23 +1,34 @@
-use comet_bft_rpc::HttpCometBFTRpcClientFactory;
+use comet_bft_rpc::{CometBftRpcFactory, HttpCometBFTRpcClientFactory};
 use tendermint_light_client::{
-    builder::LightClientBuilder, instance::Instance, light_client::LightClient,
+    builder::LightClientBuilder,
+    instance::Instance,
+    light_client::LightClient,
+    store::{memory::MemoryStore, LightStore},
 };
 
+/// Builds a light client from a comet bft rpc client factory and a light store
 pub(crate) struct LightCBFTClientBuilder {
+    /// The rpc client factory
     rpc_client_factory: HttpCometBFTRpcClientFactory,
+    /// The light store, best to use [MemoryStore] from the tendermint_light_client crate
     light_store: Box<dyn LightStore>,
 }
 
 impl LightCBFTClientBuilder {
-    pub fn new(
-        rpc_client_factory: HttpCometBFTRpcClientFactory,
-        light_store: Box<dyn LightStore>,
-    ) -> Self {
+    pub fn new(rpc_client_factory: HttpCometBFTRpcClientFactory) -> Self {
+        let light_store = Box::new(MemoryStore::new());
+
         Self { rpc_client_factory, light_store }
     }
 
+    pub fn with_light_store(mut self, light_store: Box<dyn LightStore>) -> Self {
+        self.light_store = light_store;
+        self
+    }
+
     pub async fn build(self) -> Instance {
-        let rpc_client = self.rpc_client_factory.create_client().unwrap();
+        let rpc_client =
+            self.rpc_client_factory.build_and_connect().expect("should connect to RPC client");
         let node_id = rpc_client.status().await.expect("Failed to get node info").node_info.id;
         let light_client = LightClientBuilder::prod(
             node_id,
@@ -28,6 +39,10 @@ impl LightCBFTClientBuilder {
         );
 
         let light = light_client.trust_from_store().unwrap().build();
+        light
+            .light_client
+            .verify_to_highest(&mut light.state)
+            .expect("Failed to verify light client");
 
         light
     }
