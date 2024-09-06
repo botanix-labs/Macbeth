51e01c0bd6ac55104da6f7dd7af5de5be37a4f51
Scott Millner <22253830+scottmillner@users.noreply.github.com>
2024-09-06 11:12:29 -0600

    test(cbft): add test for test_transactions_signed_from_bytes

--- crates/consensus/authority/src/comet_bft/utils.rs
+++ crates/consensus/authority/src/comet_bft/utils.rs
@@ -20,3 +20,44 @@ pub fn transactions_signed_from_bytes(
 
     Ok(txs)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use reth_primitives::TransactionSigned;
+    use std::io::Cursor;
+    use tendermint_rpc::endpoint::tx;
+
+    #[test]
+    fn test_transactions_signed_from_bytes() {
+        let mut tx1 = TransactionSigned::default();
+        tx1.transaction.set_nonce(1);
+        let mut tx2 = TransactionSigned::default();
+        tx2.transaction.set_nonce(2);
+        let mut tx3 = TransactionSigned::default();
+        tx3.transaction.set_nonce(3);
+
+        let mut buf1 = Vec::new();
+        tx1.encode_enveloped(&mut buf1);
+        let signed_tx1 = TransactionSigned::decode_enveloped(&mut buf1.as_slice()).unwrap();
+        let bytes1 = prost::bytes::Bytes::copy_from_slice(buf1.as_slice());
+
+        let mut buf2 = Vec::new();
+        tx2.encode_enveloped(&mut buf2);
+        let signed_tx2 = TransactionSigned::decode_enveloped(&mut buf2.as_slice()).unwrap();
+        let bytes2 = prost::bytes::Bytes::copy_from_slice(buf2.as_slice());
+
+        let mut buf3 = Vec::new();
+        tx3.encode_enveloped(&mut buf3);
+        let signed_tx3 = TransactionSigned::decode_enveloped(&mut buf3.as_slice()).unwrap();
+        let bytes3 = prost::bytes::Bytes::copy_from_slice(buf3.as_slice());
+
+        let vec_bytes = vec![bytes1, bytes2, bytes3];
+        let txs = transactions_signed_from_bytes(vec_bytes.iter().cloned()).unwrap();
+
+        assert_eq!(txs.len(), 3);
+        assert_eq!(txs[0], signed_tx1);
+        assert_eq!(txs[1], signed_tx2);
+        assert_eq!(txs[2], signed_tx3);
+    }
+}
