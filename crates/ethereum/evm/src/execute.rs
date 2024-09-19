bf623f053c2903c3c1d611025d20a3cc2c0eacc1
Scott Millner <22253830+scottmillner@users.noreply.github.com>
2024-09-19 10:08:34 -0600

    test(main): fix test block_gas_limit_error

--- crates/ethereum/evm/src/execute.rs
+++ crates/ethereum/evm/src/execute.rs
@@ -1650,6 +1650,9 @@ mod tests {
         header.receipts_root =
             b256!("b31a3e47b902e9211c4d349af4e4c5604ce388471e79ca008907ae4616bb0ed3");
 
+        let edh = ExtraDataHeader::default();
+        header.add_extra_data_header(&edh);
+
         // Create a transaction with a gas limit higher than the block gas limit
         let tx = sign_tx_with_key_pair(
             sender_key_pair,
