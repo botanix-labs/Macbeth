6077d5493e3c8f20f7ceeddd61c60279becfa198
Armin Sabouri <armins88@gmail.com>
2024-09-27 15:49:18 -0400

    test: update dkg flow test util to send round2dkg messages to each participant

--- bin/test-suite/src/suite/consensus/frost/test_dkg.rs
+++ bin/test-suite/src/suite/consensus/frost/test_dkg.rs
@@ -3,9 +3,16 @@ use crate::{it_error_print, suite::consensus::ConsensusIntegrationTestSuite};
 use bitcoin::{consensus::Encodable, Address, Amount};
 use btcserverlib::pegout_id::PegoutId;
 use client::{self, BtcServerClient};
+use frost_secp256k1_tr as frost;
 use rand::{rngs::StdRng, RngCore, SeedableRng};
-use std::{str::FromStr, vec};
+use std::{collections::BTreeMap, str::FromStr, vec};
 use tonic::transport::Channel;
+
+macro_rules! frost_id {
+    ($index:expr) => {
+        frost::Identifier::derive($index.to_le_bytes().as_slice()).expect("valid id")
+    };
+}
 
 pub async fn dkg_flow(suite: &ConsensusIntegrationTestSuite) -> Result<(), Error> {
     // create btc server clients
@@ -59,6 +66,12 @@ pub async fn dkg_flow(suite: &ConsensusIntegrationTestSuite) -> Result<(), Error
 }
 
 pub async fn do_dkg(clients: &mut [client::BtcServerClient<Channel>]) -> Result<(), Error> {
+    // creating a mapping of client index to fronst identifier
+    let mut frost_id_map = BTreeMap::new();
+    for (i, _) in clients.iter().enumerate() {
+        let frost_id = frost_id!(i as u16);
+        frost_id_map.insert(frost_id, i);
+    }
     // Round 1 dkg
     let mut round1_packages = vec![];
     for c in clients.iter_mut() {
@@ -107,17 +120,27 @@ pub async fn do_dkg(clients: &mut [client::BtcServerClient<Channel>]) -> Result<
         }
     }
 
-    // Send round 2 dkg packages to each respective participant
-    for (i, c) in clients.iter_mut().enumerate() {
-        for (j, p) in round2_packages.iter().enumerate() {
-            if i != j {
-                c.new_round2_dkg_package(tonic::Request::new(client::DkgPayload {
-                    identifier: p.identifier.clone(),
-                    payload: p.payload.clone(),
+    // Send dkg round2 shares to each recepient
+    for (i, p) in round2_packages.iter().enumerate() {
+        let from_frost_id = frost_id!(i as u16).serialize().to_vec();
+        let round2_shares = p.payload.clone();
+        let shares = serde_json::from_slice::<
+            BTreeMap<frost::Identifier, frost::keys::dkg::round2::Package>,
+        >(&round2_shares)
+        .expect("Failed to deserialize round 2 shares");
+
+        // there should always be n-1 shares
+        assert_eq!(shares.len(), clients.len() - 1);
+        for (j, (identifier, payload)) in shares.iter().enumerate() {
+            let client_index = frost_id_map.get(&identifier).unwrap();
+            let mut client = clients[*client_index].clone();
+            client
+                .new_round2_dkg_package(tonic::Request::new(client::DkgPayload {
+                    identifier: from_frost_id.clone(),
+                    payload: serde_json::to_vec(&payload).unwrap(),
                 }))
                 .await
                 .map_err(Error::Request)?;
-            }
         }
     }
     Ok(())
