2bd845a8123bbdc7ff11efdaedf88cec4fa6d930
Scott Millner <22253830+scottmillner@users.noreply.github.com>
2024-12-26 11:43:32 -0700

    feature(btc-server): add int test test_round1_then_new_signing_session()

--- bin/test-suite/src/suite/consensus/frost/mod.rs
+++ bin/test-suite/src/suite/consensus/frost/mod.rs
@@ -10,6 +10,7 @@ pub mod test_frost_e2e;
 pub mod test_frost_e2e_signing_disconnect;
 pub mod test_mempool_gossip;
 pub mod test_pending_pegouts;
+pub mod test_round1_then_new_signing_session;
 pub mod test_signing;
 pub mod test_utxo_commitment;
 pub mod test_utxo_sync;
