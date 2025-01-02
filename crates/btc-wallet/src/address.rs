466cbd76e2495020616a6f6d7e09aafd9531e344
Armin Sabouri <armins88@gmail.com>
2025-01-02 12:20:33 -0500

    chore(btc_wallet): remove deprecated functions

--- crates/btc-wallet/src/address.rs
+++ crates/btc-wallet/src/address.rs
@@ -1,11 +1,5 @@
-use std::io::Write;
-
-use bitcoin::{
-    hashes::{sha256, Hash},
-    key::TweakedPublicKey,
-    Address, Network, ScriptBuf,
-};
-use secp256k1::{PublicKey, Scalar, Secp256k1, SecretKey, Verification};
+use bitcoin::{key::TweakedPublicKey, Address, Network, ScriptBuf};
+use secp256k1::PublicKey;
 
 pub trait EthAddress {
     fn as_slice(&self) -> &[u8];
@@ -30,51 +24,6 @@ pub fn generate_taproot_address(tweaked_public_key: &PublicKey, network: Network
         TweakedPublicKey::dangerous_assume_tweaked(tweaked_public_key.x_only_public_key().0);
     let p2tr_script = bitcoin::ScriptBuf::new_p2tr_tweaked(tweaked_pk);
     Address::from_script(&p2tr_script, network).expect("valid address")
-}
-
-/// Deprecated
-fn generate_tweak<T>(eth_address: &T, aggregate_key: &PublicKey) -> Scalar
-where
-    T: EthAddress,
-{
-    let eth = eth_address.as_slice();
-    let eth_address_tweak = sha256::Hash::hash(eth);
-    {
-        let mut eng = sha256::Hash::engine();
-        eng.write_all(&aggregate_key.serialize()).unwrap();
-        eng.write_all(&eth_address_tweak[..]).unwrap();
-        let hash = sha256::Hash::from_engine(eng);
-        secp256k1::Scalar::from_be_bytes(hash.to_byte_array())
-            .expect("safe hash values should be under the curve order")
-    }
-}
-
-/// Deprecated
-pub fn generate_tweaked_public_key<T>(
-    secp: &Secp256k1<impl Verification>,
-    eth_address: &T,
-    aggregate_key: &PublicKey,
-) -> secp256k1::PublicKey
-where
-    T: EthAddress,
-{
-    let tweak = generate_tweak(eth_address, aggregate_key);
-    aggregate_key
-        .add_exp_tweak(secp, &tweak)
-        .expect("if you hash the point into the tweak, this can't really happen")
-}
-
-/// Deprecated
-pub fn generate_tweaked_secret_key<T>(
-    eth_address: &T,
-    aggregate_key: &PublicKey,
-    secret_key: &SecretKey,
-) -> SecretKey
-where
-    T: EthAddress,
-{
-    let tweak = generate_tweak(eth_address, aggregate_key);
-    secret_key.add_tweak(&tweak).expect("legal tweak")
 }
 
 pub fn generate_taproot_change_scriptpubkey(public_key: &PublicKey) -> ScriptBuf {
