9954eabef6260fc8adfd266ba6fb17beea198181
Fabio Lama <fabio.lama@pm.me>
2025-04-15 12:41:16 +0000

    remove unused authority/src/test_utils.rs

--- crates/consensus/authority/src/lib.rs
+++ crates/consensus/authority/src/lib.rs
@@ -56,8 +56,6 @@ pub mod utils;
 pub use builder::AuthorityConsensusBuilder;
 pub mod metrics;
 pub mod random_source_provider;
-/// Expose test utils for unit testing
-pub mod test_utils;
 
 /// Max EDH size; for specific details see [ExtraDataHeader]
 pub const MAX_EDH_SIZE: usize = 93;
