82ca04ebcaf3abba3c4a12fbb274d58366c92bf5
Scott Millner <scottmillner237@gmail.com>
2025-04-29 19:00:10 -0600

    refactor(genesis): update hot wallet address for mainnet

--- crates/primitives-traits/src/constants/mod.rs
+++ crates/primitives-traits/src/constants/mod.rs
@@ -122,9 +122,9 @@ pub const HOLESKY_GENESIS_HASH: B256 =
 pub const DEV_GENESIS_HASH: B256 =
     b256!("2f980576711e3617a5e4d83dd539548ec0f7792007d505a3d2e9674833af2d7c");
 
-/// Botanix Mainnet genesis hash.
+/// Botanix Mainnet genesis hash: `0x0210ae550e730d0e18f96896b80caad6f59dcc0b83b67421975716d155d027c6`
 pub const BOTANIX_MAINNET_GENESIS: B256 =
-    b256!("a6cb75c8079e6ef36acfd73c613391a087ef4ee7c188fe2b564aad8964b01542");
+    b256!("0210ae550e730d0e18f96896b80caad6f59dcc0b83b67421975716d155d027c6");
 
 /// Botanix Testnet genesis hash.
 pub const BOTANIX_TESTNET_GENESIS: B256 =
