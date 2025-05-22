91070696ec14516a136abedfe6f29ce98e1f1ef6
Ivan Shumkov <ivanshumkov@gmail.com>
2025-05-22 20:33:08 +0700

    fix(poa): empty payload panic logs

--- bin/reth/src/commands/poa/mod.rs
+++ bin/reth/src/commands/poa/mod.rs
@@ -104,10 +104,21 @@ use crate::{
 /// Adds a panic hook to log the panic information
 pub fn set_panic_hook() {
     std::panic::set_hook(Box::new(|panic_info| {
-        let payload = panic_info.payload().downcast_ref::<&str>().cloned().unwrap_or_default();
+        let payload = panic_info.payload();
+
+        #[allow(clippy::manual_map)]
+        let payload = if let Some(s) = payload.downcast_ref::<&str>() {
+            Some(&**s)
+        } else if let Some(s) = payload.downcast_ref::<String>() {
+            Some(s.as_str())
+        } else {
+            None
+        };
+
         let location = panic_info.location().map(|l| l.to_string());
 
-        tracing::error!(panic.payload = payload, panic.location = location, "Uncaught panic");
+        error!(panic.payload = payload, panic.location = location, "Uncaught panic");
+
         std::process::exit(1);
     }));
 }
