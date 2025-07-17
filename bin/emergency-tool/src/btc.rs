7f553c8c26fde1350147a7f916bcb3ef297f74aa
SatwikPrabhu <satwik123.sp@gmail.com>
2025-07-17 09:20:02 -0700

    Feat(wallet sweeping): Import/export utxo set and comparison

--- bin/emergency-tool/src/btc.rs
+++ bin/emergency-tool/src/btc.rs
@@ -1,6 +1,15 @@
 use crate::config::Config;
 use anyhow::Result;
+use bitcoin::{Amount, Txid};
 use bitcoincore_rpc::{Auth, Client, RpcApi};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, Hash)]
+pub struct Utxo {
+    pub txid: Txid,
+    pub vout: u32,
+    pub amount: Amount,
+}
 
 pub struct BtcClient {
     client: Client,
@@ -18,8 +27,16 @@ impl BtcClient {
         Ok(Self { client: rpc })
     }
 
-    pub async fn get_utxos(&self) -> Result<Vec<bitcoincore_rpc::json::ListUnspentResultEntry>> {
+    pub async fn get_utxos(&self) -> Result<Vec<Utxo>> {
         let utxos = self.client.list_unspent(None, None, None, None, None)?;
+        let utxos = utxos
+            .into_iter()
+            .map(|u| Utxo {
+                txid: u.txid,
+                vout: u.vout,
+                amount: u.amount,
+            })
+            .collect();
         Ok(utxos)
     }
 } 
--- bin/emergency-tool/src/main.rs
+++ bin/emergency-tool/src/main.rs
@@ -1,4 +1,12 @@
+use std::{
+    collections::HashSet,
+    fs::File,
+    io::{Read, Write},
+    path::PathBuf,
+};
+
 use anyhow::Result;
+use btc::Utxo;
 use clap::{Parser, Subcommand};
 use tracing_subscriber::EnvFilter;
 
@@ -28,6 +36,18 @@ pub enum Commands {
 pub enum UtxoCommands {
     /// Lists all UTXOs for the federation's known addresses
     List,
+    /// Exports all UTXOs for the federation's known addresses to a file
+    Export {
+        /// The path to the file to export UTXOs to. If not provided, will print to stdout.
+        #[clap(short, long)]
+        output: Option<PathBuf>,
+    },
+    /// Compares the local UTXO set against an imported file
+    Compare {
+        /// The path to the file to import UTXOs from.
+        #[clap(short, long)]
+        input: PathBuf,
+    },
 }
 
 #[tokio::main]
@@ -45,6 +65,46 @@ async fn main() -> Result<()> {
                 let btc_client = BtcClient::new(&config)?;
                 let utxos = btc_client.get_utxos().await?;
                 println!("{:#?}", utxos);
+            }
+            UtxoCommands::Export { output } => {
+                let btc_client = BtcClient::new(&config)?;
+                let utxos = btc_client.get_utxos().await?;
+                let utxos_json = serde_json::to_string_pretty(&utxos)?;
+                if let Some(output_path) = output {
+                    let mut file = File::create(output_path)?;
+                    file.write_all(utxos_json.as_bytes())?;
+                } else {
+                    println!("{}", utxos_json);
+                }
+            }
+            UtxoCommands::Compare { input } => {
+                let btc_client = BtcClient::new(&config)?;
+                let local_utxos = btc_client.get_utxos().await?;
+                let local_utxos: HashSet<Utxo> = local_utxos.into_iter().collect();
+
+                let mut file = File::open(input)?;
+                let mut contents = String::new();
+                file.read_to_string(&mut contents)?;
+                let remote_utxos: Vec<Utxo> = serde_json::from_str(&contents)?;
+                let remote_utxos: HashSet<Utxo> = remote_utxos.into_iter().collect();
+
+                let in_local_not_remote: Vec<_> =
+                    local_utxos.difference(&remote_utxos).collect();
+                let in_remote_not_local: Vec<_> =
+                    remote_utxos.difference(&local_utxos).collect();
+
+                if in_local_not_remote.is_empty() && in_remote_not_local.is_empty() {
+                    println!("UTXO sets are identical.");
+                } else {
+                    if !in_local_not_remote.is_empty() {
+                        println!("UTXOs present locally but not in remote set:");
+                        println!("{:#?}", in_local_not_remote);
+                    }
+                    if !in_remote_not_local.is_empty() {
+                        println!("UTXOs present in remote set but not locally:");
+                        println!("{:#?}", in_remote_not_local);
+                    }
+                }
             }
         },
     }
