1f221f1092151a811ee961fc88953de73e5a80b5
Darius Parvin <darius@botanixlabs.xyz>
2025-08-09 10:23:52 -0700

    refactor(authority): utxo recovery file json to use camelCase

--- bin/reth/src/commands/poa/mod.rs
+++ bin/reth/src/commands/poa/mod.rs
@@ -142,6 +142,7 @@ struct UtxosRecoveryConfig {
 }
 
 #[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
 struct UtxoRecoveryData {
     /// Transaction ID as hex string
     txid: String,
@@ -1262,7 +1263,9 @@ mod tests {
     use reth_discv4::DEFAULT_DISCOVERY_PORT;
     use reth_node_core::args::{utils::get_botanix_chain, FedMemberPubKey, FederationTomlConfig};
     use std::{
-        io::Write, net::{IpAddr, Ipv4Addr}, path::Path
+        io::Write,
+        net::{IpAddr, Ipv4Addr},
+        path::Path,
     };
     use tempfile::NamedTempFile;
 
@@ -1579,12 +1582,12 @@ mod tests {
             {
                 "txid": "1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef",
                 "vout": 0,
-                "eth_address": "0x742d35cc6554c8532b5fd1b61cdb58d5c5c5e0c2"
+                "ethAddress": "0x742d35cc6554c8532b5fd1b61cdb58d5c5c5e0c2"
             },
             {
                 "txid": "fedcba0987654321fedcba0987654321fedcba0987654321fedcba0987654321",
                 "vout": 1,
-                "eth_address": ""
+                "ethAddress": ""
             }
         ]"#;
         temp_file.write_all(json_content.as_bytes()).unwrap();
