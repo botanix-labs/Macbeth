a9605cb2bf9bccd914b1e5fece84329469e9079a
Darius Parvin <41220998+DariusParvin@users.noreply.github.com>
2025-08-12 10:51:34 -0700

    feat(reth): add missing utxos on reth startup (#911)
    Co-authored-by: scootay <scottmillner237@gmail.com>

--- crates/botanix-cli-args/src/poa_node.rs
+++ crates/botanix-cli-args/src/poa_node.rs
@@ -98,4 +98,13 @@ pub struct PoaNodeArgs {
         value_parser = parse_ethereum_address,
     )]
     pub block_fee_recipient_address: Option<Address>,
+
+    /// Path to JSON file containing UTXOs to recover
+    #[arg(
+        long,
+        value_name = "UTXO_RECOVERY_FILE",
+        env = "RETH_UTXO_RECOVERY_FILE",
+        verbatim_doc_comment
+    )]
+    pub utxo_recovery_file: Option<PathBuf>,
 }
