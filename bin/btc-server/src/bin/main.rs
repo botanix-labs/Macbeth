681e7f2f5e66f5442b40aaa9da710d4c570efb42
Darius Parvin <darius@botanixlabs.xyz>
2025-08-28 12:43:49 -0700

    fix/refactor(btc-server): fix endian conversion by using proper bitcoin OutPoint conversion

--- bin/btc-server/src/bin/main.rs
+++ bin/btc-server/src/bin/main.rs
@@ -14,8 +14,7 @@ use std::{
 use alloy_rpc_types_engine::{JwtError, JwtSecret};
 use base64::{engine::general_purpose, Engine};
 use bitcoin::{
-    consensus::Decodable,
-    secp256k1, Amount, BlockHash, Psbt, ScriptBuf, Transaction, TxOut,
+    consensus::Decodable, secp256k1, Amount, BlockHash, Psbt, ScriptBuf, Transaction, TxOut,
 };
 use bitcoin_hashes::Hash;
 use bitcoincore_rpc::{Auth, RpcApi};
