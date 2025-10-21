278315b127105ec3a83a5a7d3a47e818a114a477
Scott Millner <scottmillner237@gmail.com>
2025-10-21 15:48:55 -0600

    chore: remove feature doc_auto_cfg

--- crates/ethereum/consensus/src/lib.rs
+++ crates/ethereum/consensus/src/lib.rs
@@ -6,7 +6,7 @@
     issue_tracker_base_url = "https://github.com/paradigmxyz/reth/issues/"
 )]
 #![cfg_attr(not(test), warn(unused_crate_dependencies))]
-#![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
+#![cfg_attr(docsrs, feature(doc_cfg))]
 
 use reth_chainspec::{ChainSpec, EthereumHardfork, EthereumHardforks};
 use reth_consensus::{Consensus, ConsensusError, PostExecutionInput};
